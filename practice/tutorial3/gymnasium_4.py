10a7ab562991e9f15957e488d472f09ebb285b6c
hollyriver <hcssk2800@gmail.com>
2026-02-15 17:15:56 +0900

    .

--- practice/tutorial3/gymnasium_4.py
+++ practice/tutorial3/gymnasium_4.py
@@ -3,7 +3,7 @@ from gymnasium.wrappers import RecordVideo
 
 env = gym.make('CartPole-v1', render_mode="rgb_array")  ## 영상을 파일로 저장 시 사용하는 렌더링 모드
 ## wrapper로 env 객체 감싸기, 10개 에피소드마다 녹화 (에피소드 단위)
-env = RecordVideo(env, video_folder='./tmp/cartpole-experiment-1', episode_trigger=lambda count: count % 10 == 0)
+env = RecordVideo(env, video_folder='./cartpole-experiment-1', episode_trigger=lambda count: count % 10 == 0)
 
 for i_episode in range(20):
     observation, info = env.reset()
